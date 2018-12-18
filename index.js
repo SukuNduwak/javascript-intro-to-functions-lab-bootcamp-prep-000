a50045598b9fb4ca86792dcd2578b2fd2b4b4045
Barnabas Nduwak Suku <barnssuku@gmail.com>
2018-12-18 14:53:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(word) {
     return "I can't hear you!";
   } else if (word === word.toUpperCase()) {
     return "YES INDEED!";
-  } else if (word === "I love you, Grandma") {
-    return "I love you, too";
+  } else if (word === "I love you, Grandma.") {
+    return "I love you, too.";
   }
 }
